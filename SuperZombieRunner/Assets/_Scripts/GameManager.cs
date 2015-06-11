b97060de1c1261fc4d160bd7f9b2cd53f2368eb2
kieronlanning <kieron@outlook.com>
2015-06-11 21:45:57 +0100

    Up to 35. About to add a UI.

--- SuperZombieRunner/Assets/_Scripts/GameManager.cs
+++ SuperZombieRunner/Assets/_Scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : MonoBehaviour
 {
     public GameObject playerPrefab;
 
+    bool _gameStarted;
+    TimeManager _timeManager;
     GameObject _player;
     GameObject _floor;
     Spawner _spawner;
@@ -13,6 +15,7 @@ public class GameManager : MonoBehaviour
     {
         _floor = GameObject.Find("Foreground");
         _spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        _timeManager = GetComponent<TimeManager>();
     }
 
     void Start()
@@ -27,16 +30,30 @@ public class GameManager : MonoBehaviour
 
         _spawner.active = false;
 
-        ResetGame();
+        Time.timeScale = 0f;
+    }
+
+    void Update()
+    {
+        if (!_gameStarted && Time.timeScale == 0f)
+        {
+            if (Input.anyKeyDown)
+            {
+                _timeManager.ManipulateTime(1f, 1f);
+                ResetGame();
+            }
+        }
     }
 
     void ResetGame()
     {
         _spawner.active = true;
-        _player = GameObjectUtility.Instantiate(playerPrefab, new Vector3(0, (Screen.height / PixelPerfectCamera.pixelToUnits) / 2, 0));
+        _player = GameObjectUtility.Instantiate(playerPrefab, new Vector3(0, ((Screen.height / PixelPerfectCamera.pixelToUnits) / 2) + 100, 0));
 
         var playerDestroyScript = _player.GetComponent<DestoryOffScreen>();
         playerDestroyScript.DestroyCallback += OnPlayerKilled;
+
+        _gameStarted = true;
     }
 
     void OnPlayerKilled()
@@ -47,5 +64,9 @@ public class GameManager : MonoBehaviour
         playerDestroyScript.DestroyCallback -= OnPlayerKilled;
 
         _player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        _timeManager.ManipulateTime(0, 5.5f);
+
+        _gameStarted = false;
     }
 }
---
+++ SuperZombieRunner/Assets/_Scripts/TimeManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager : MonoBehaviour
+{
+    public void ManipulateTime(float newTime, float duration)
+    {
+        if (Time.timeScale == 0f)
+            Time.timeScale = .1f;
+
+        StartCoroutine(FadeTo(newTime, duration));
+    }
+
+    IEnumerator FadeTo(float value, float time)
+    {
+        // Using Time.deltaTime / time means it will stop smoothly based on game time, not as fast as it can be executed.
+
+        for (var t = 0f; t < 1f; t += Time.deltaTime/time)
+        {
+            Time.timeScale = Mathf.Lerp(Time.timeScale, value, t);
+
+            if (Mathf.Abs(value - Time.timeScale) < .01f)
+            {
+                Time.timeScale = value;
+                return false;
+            }
+
+            yield return null;
+        }
+    }
+}
